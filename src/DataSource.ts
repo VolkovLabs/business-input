40edc68315e79c9a72093a9470d1accdadf9b77c
Marcus Olsson <marcus.olsson@hey.com>
2020-09-06 19:46:24 +0200

    Interpolate variables for string fields

--- src/DataSource.ts
+++ src/DataSource.ts
@@ -1,5 +1,15 @@
-import { DataQueryRequest, DataQueryResponse, DataSourceApi, DataSourceInstanceSettings } from '@grafana/data';
+import {
+  DataQueryRequest,
+  DataQueryResponse,
+  DataSourceApi,
+  DataSourceInstanceSettings,
+  toDataFrame,
+  DataFrame,
+  FieldType,
+  ArrayVector,
+} from '@grafana/data';
 
+import { getTemplateSrv } from '@grafana/runtime';
 import { StaticQuery, StaticDataSourceOptions } from './types';
 
 export class DataSource extends DataSourceApi<StaticQuery, StaticDataSourceOptions> {
@@ -8,11 +18,27 @@ export class DataSource extends DataSourceApi<StaticQuery, StaticDataSourceOptio
   }
 
   async query(options: DataQueryRequest<StaticQuery>): Promise<DataQueryResponse> {
+    // Interpolate variables in string fields.
+    const interpolateVariables = (frame: DataFrame) => {
+      for (let i = 0; i < frame.fields.length; i++) {
+        const field = frame.fields[i];
+
+        // Skip non-text fields.
+        if (field.type === FieldType.string) {
+          field.values = new ArrayVector(field.values.toArray().map(_ => getTemplateSrv().replace(_, {}, 'csv')));
+        }
+
+        frame.fields[i] = field;
+      }
+      return frame;
+    };
+
     return {
       data: options.targets
         .filter(target => !target.hide)
         .filter(target => target.frame)
-        .map(target => target.frame),
+        .map(target => toDataFrame(target.frame))
+        .map(interpolateVariables),
     };
   }
 
