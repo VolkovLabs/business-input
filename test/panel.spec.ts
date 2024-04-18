371ec18853c843429e4f2156f3a0ffde07afb2e2
vitPinchuk <vit.pinchuck@gmail.com>
2024-04-18 11:05:03 +0300

    E2E updated

--- test/panel.spec.ts
+++ test/panel.spec.ts
@@ -1,5 +1,29 @@
 import { test, expect } from '@grafana/plugin-e2e';
 
 test.describe('Static Data Source', () => {
-  // TODO
+  test('Viewing a panel with a Logs from Static Data', async ({ gotoDashboardPage, dashboardPage, page }) => {
+    /**
+     * Go To panels dashboard panels.json
+     * return dashboardPage
+     */
+    await gotoDashboardPage({ uid: 'O4tc_E6Gz' });
+
+    /**
+     * Await content load
+     */
+    await page.waitForTimeout(1000);
+
+    /**
+     * Find panel by title with chart
+     * Should be visible
+     */
+    await expect(dashboardPage.getPanelByTitle('Logs').locator).toBeVisible();
+
+    /**
+     * Check and compare image
+     */
+    await expect(dashboardPage.getPanelByTitle('Logs').locator).toHaveScreenshot('actual-screenshot.png', {
+      maxDiffPixelRatio: 0.1,
+    });
+  });
 });
