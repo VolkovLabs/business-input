5c20a6069423a17612c299bcca78ed8a42bc2e25
Vitaly Pinchuk <146737590+vitPinchuk@users.noreply.github.com>
2024-04-22 13:44:04 +0300

    Prepare for Grafana 11 (#67)
    * ft: removed MutableDataFrame, ArrayVector, toArray
    
    * updated test cases for convertToDataFrame

--- src/utils/frame.test.ts
+++ src/utils/frame.test.ts
@@ -1,6 +1,6 @@
 import { FieldType } from '@grafana/data';
 
-import { prepareModel } from './frame';
+import { convertToDataFrame, prepareModel } from './frame';
 
 jest.mock('uuid', () => ({
   v4: jest.fn(() => '123456'),
@@ -43,6 +43,119 @@ describe('Frame Utils', () => {
         ],
       };
       expect(prepareModel(dataFrame).rows).toEqual([]);
+    });
+  });
+
+  /**
+   * convertToDataFrame
+   */
+  describe('convertToDataFrame', () => {
+    it('should map rows and verify field values', () => {
+      /**
+       * Model
+       */
+      const model = {
+        meta: {},
+        fields: [
+          {
+            name: 'Field',
+            type: 'string',
+            id: '053c4d18-9954-4fea-b8ef-83eb2da95147',
+          },
+          {
+            name: 'value',
+            type: 'number',
+            id: '5a544f7c-27ff-4042-bcb7-2873e1eef6c3',
+          },
+        ],
+        rows: [
+          {
+            value: ['Test 1', 10],
+            id: '12380cd9-9ddf-4a26-bdae-87bfcaf590e4',
+          },
+          {
+            value: ['Test 2', 151],
+            id: 'df1f7a2b-2fc4-40a4-a88b-d661079131bb',
+          },
+        ],
+      };
+
+      const frame = convertToDataFrame(model as any);
+
+      /**
+       *  Should return correct values
+       */
+      expect(frame.fields[0].values).toEqual(['Test 1', 'Test 2']);
+      expect(frame.fields[1].values).toEqual([10, 151]);
+    });
+
+    it('should handle fields without values', () => {
+      /**
+       * Model
+       */
+      const model = {
+        meta: {},
+        fields: [
+          {
+            name: 'Field',
+            type: 'string',
+            id: '053c4d18-9954-4fea-b8ef-83eb2da95147',
+          },
+          {
+            name: 'value',
+            type: 'number',
+            id: '5a544f7c-27ff-4042-bcb7-2873e1eef6c3',
+          },
+        ],
+        rows: [],
+      };
+
+      const frame = convertToDataFrame(model as any);
+
+      /**
+       *  Should return correct values
+       */
+      expect(frame.fields[0].values).toEqual([]);
+      expect(frame.fields[1].values).toEqual([]);
+    });
+
+    it('should handle fields if types different', () => {
+      /**
+       * Model
+       */
+      const model = {
+        meta: {},
+        fields: [
+          {
+            name: 'Field',
+            type: 'string',
+            id: '053c4d18-9954-4fea-b8ef-83eb2da95147',
+          },
+          {
+            name: 'value',
+            type: 'boolean',
+            id: '5a544f7c-27ff-4042-bcb7-2873e1eef6c3',
+          },
+        ],
+        rows: [
+          {
+            value: ['Test 1', 10],
+            id: '12380cd9-9ddf-4a26-bdae-87bfcaf590e4',
+          },
+          {
+            value: ['Test 2', 151],
+            id: 'df1f7a2b-2fc4-40a4-a88b-d661079131bb',
+          },
+        ],
+      };
+
+      const frame = convertToDataFrame(model as any);
+
+      /**
+       *  Should return correct values
+       */
+      expect(frame.fields[0].values).toEqual(['Test 1', 'Test 2']);
+      expect(frame.fields[1].values).toEqual([null, null]);
     });
   });
 });
